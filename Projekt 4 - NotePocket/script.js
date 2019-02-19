f8a5a3d1b13719895877fc2daa5d88bf8d6c54d9
KrystianJacaszek <krystian.jacaszek@gmail.com>
2019-02-19 23:57:27 +0100

    Usprawnienia w dzialaniu

--- Projekt 4 - NotePocket/script.js
+++ Projekt 4 - NotePocket/script.js
@@ -4,7 +4,7 @@ function basicFunc(){
     
 }
 
-let stickyArray=[,];
+let stickyArray=[];
 
 function zapisz(){
 
@@ -16,7 +16,7 @@ function zapisz(){
 function addNotes(){
 
     let notesBody=document.querySelector("#notesBody")
-    let el =new Note("","")
+    let el =new Note("sd","d")
     //stickyArray.push(el)
     notesBody.appendChild(el)
 
@@ -31,96 +31,107 @@ function showArray(){
     console.log(stickyArray)
 }
 
-function Note(text, title) {
+function Note(_text, _title) {
 
-    createStickyEl()
-    createLiseners()
-    
-    this.text=text
-    this.title=title
+    let ob = new createStickyEl()
 
-    setData(text,title)
+    setData(_title,_text)
 
-    function setData(text,title){
-        this.stickyText.value=text
-        this.stickyTitle.value=title
+    function setData(title,text){
+        stickyArray.push([title,text]);
+        ob.ArrayIndex=(stickyArray.length-1)
+        ob.title=title
+        ob.text=text
+        ob.stickyText.value=text
+        ob.stickyTitle.value=title
 
     }
-   
+
     function createStickyEl(){
+        this.title=""
+        this.text=""
+        this.ArrayIndex
+        this.colorId=0
+
         this.sticky = document.createElement('div')
-        sticky.className="stickyBase"
+        this.sticky.className="stickyBase"
 
         this.stickyConainerTop=document.createElement('div')
-        stickyConainerTop.className="stickyContainerTop"
-        sticky.appendChild(stickyConainerTop)
+        this.stickyConainerTop.className="stickyContainerTop"
+        this.sticky.appendChild(this.stickyConainerTop)
     
         this.stickyDelete = document.createElement('i')
-        stickyDelete.className="far fa-calendar-times stickyButtons"
-        stickyConainerTop.appendChild(stickyDelete)
+        this.stickyDelete.className="far fa-calendar-times stickyButtons"
+        this.stickyConainerTop.appendChild(this.stickyDelete)
     
         this.stickyTitle = document.createElement('input')
-        stickyTitle.className="stickyTitle"
-        stickyTitle.setAttribute('type', 'text');
-        stickyTitle.setAttribute('maxlength', '15');
-        stickyConainerTop.appendChild(stickyTitle)
+        this.stickyTitle.className="stickyTitle"
+        this.stickyTitle.setAttribute('type', 'text');
+        this.stickyTitle.setAttribute('maxlength', '15');
+        this.stickyConainerTop.appendChild(this.stickyTitle)
     
         this.stickyChangeColor = document.createElement('i')
-        stickyChangeColor.className="fas fa-fill-drip stickyButtons"
-        stickyConainerTop.appendChild(stickyChangeColor)
+        this.stickyChangeColor.className="fas fa-fill-drip stickyButtons"
+        this.stickyConainerTop.appendChild(this.stickyChangeColor)
     
         this.stickyText = document.createElement('textarea')
-        stickyText.setAttribute('maxlength',500)
-        stickyText.className="stickyText"
-        sticky.appendChild(stickyText);
+        this.stickyText.setAttribute('maxlength',500)
+        this.stickyText.className="stickyText"
+        this.sticky.appendChild(this.stickyText);
+
+        this.changeTitle=function(){
+            this.title=this.stickyTitle.value
+        }
+        this.changeText=function(){
+            this.text=this.stickyText.value
+        }
 
 
     }
 
-    function createLiseners(){
 
         let colors=["#00FA9A","#00B2EE","#FFFACD","#FFB6C1","#FFA54F","#FF6666","#EEE685"]
         let colorId=0;
     
-        sticky.style.background=colors[colorId]
+        ob.sticky.style.background=colors[colorId]
     
     
-        stickyText.addEventListener("input", function(e){
-            //alert(stickyText.value)
-            this.text=stickyText.value
-            addToArray();
-        })
-    
-        stickyTitle.addEventListener("input", function(e){
-            //alert(stickyTitle.value)
-            this.title=stickyTitle.value
-            addToArray();
-        })
-    
-    
-        stickyChangeColor.addEventListener("click", function(e){
+        ob.stickyText.addEventListener("input", function(e){
+            ob.changeText();
+            addToArray(ob.title,ob.text,ob.colorId);
            
-            if (colorId<colors.length)
-                colorId++
-            else
-                colorId=0
+        })
+        ob.stickyTitle.addEventListener("input", function(e){
+            ob.changeTitle()
+            addToArray(ob.title,ob.text,ob.colorId);
+        })
+        
     
-            sticky.style.background=colors[colorId]
+    
+        ob.stickyChangeColor.addEventListener("click", function(e){
+           
+            if (ob.colorId<colors.length)
+                ob.colorId++
+            else
+                ob.colorId=0
+    
+            ob.sticky.style.background=colors[ob.colorId]
+
         })
     
-        stickyDelete.addEventListener("click", function(e){
+        ob.stickyDelete.addEventListener("click", function(e){
             
             sticky.parentNode.removeChild(sticky)
     
         })
+    
+
+    function addToArray(title,text,colorId){
+        console.log("addToArray\ntitle: "+title+"\ntext: "+text)
+        stickyArray.splice(ob.ArrayIndex,1,[title,text,colorId]);
     }
 
-    function addToArray(){
-
-        stickyArray.push(this.text,this.title);
-    }
-
-    return sticky
+    return ob.sticky
     
 
 }
