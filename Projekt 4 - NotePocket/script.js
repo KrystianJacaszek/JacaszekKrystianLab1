69790157d825ea6f0a16cdbe746c0488ef5dc70a
KrystianJacaszek <krystian.jacaszek@gmail.com>
2019-02-20 15:44:01 +0100

    Dodana nowa funkcjonalnosc: pinUp,saveLocalStorage, code improvements

--- Projekt 4 - NotePocket/script.js
+++ Projekt 4 - NotePocket/script.js
@@ -1,47 +1,69 @@
-function basicFunc(){
-    document.querySelector("section").innerHTML+='test<br>';
-    console.log("test w konsoli");
-    
-}
-
 let stickyArray=[];
-
-function zapisz(){
-
-    localStorage.setItem
-
-
-}
 
 function addNotes(){
 
+    let el =new Note("","",0)
+
+}
+
+function saveLocal(){
+    
+    localStorage.setItem('sticky',JSON.stringify(stickyArray))
+        
+}
+
+function loadLocal(){
+
+    let stickyArray = localStorage.getItem('sticky') ? JSON.parse(localStorage.getItem('sticky')) : [];
+
+        if (stickyArray.length!=0 ){
+            for (let i=0; i<stickyArray.length;i++){
+                let el=new Note(stickyArray[i][0],
+                                stickyArray[i][1],
+                                stickyArray[i][2],)
+
+            }
+
+        }
+    
+}
+
+function refreshSticky(){
     let notesBody=document.querySelector("#notesBody")
-    let el =new Note("sd","d")
-    //stickyArray.push(el)
-    notesBody.appendChild(el)
+    while (notesBody.firstChild){
+        notesBody.removeChild(notesBody.firstChild)
+    }
+
+    stickyArray=[]
+
+    loadLocal()
 
 }
 
-function delNotes(x){
-
-
-}
 
 function showArray(){
     console.log(stickyArray)
 }
 
-function Note(_text, _title) {
+function Note(_title, _text,_color) {
+
+    let notesBody=document.querySelector("#notesBody")
 
     let ob = new createStickyEl()
 
-    setData(_title,_text)
+    let colors=["#00FA9A","#00B2EE","#FFFACD","#FFB6C1","#FFA54F","#FF6666","#EEE685"]
 
-    function setData(title,text){
-        stickyArray.push([title,text]);
+    notesBody.appendChild(ob.sticky)
+
+    setData(_title,_text,_color)
+
+    function setData(title,text,color){
+        stickyArray.push([title,text,color]);
         ob.ArrayIndex=(stickyArray.length-1)
         ob.title=title
+        ob.colorId=color
         ob.text=text
+        ob.sticky.style.background=colors[color]
         ob.stickyText.value=text
         ob.stickyTitle.value=title
 
@@ -73,6 +95,10 @@ function Note(_text, _title) {
         this.stickyChangeColor = document.createElement('i')
         this.stickyChangeColor.className="fas fa-fill-drip stickyButtons"
         this.stickyConainerTop.appendChild(this.stickyChangeColor)
+
+        this.stickyPinUp = document.createElement('i')
+        this.stickyPinUp.className="fas fa-thumbtack stickyButtons"
+        this.stickyConainerTop.appendChild(this.stickyPinUp)
     
         this.stickyText = document.createElement('textarea')
         this.stickyText.setAttribute('maxlength',500)
@@ -86,16 +112,8 @@ function Note(_text, _title) {
             this.text=this.stickyText.value
         }
 
-
     }
-
-
-        let colors=["#00FA9A","#00B2EE","#FFFACD","#FFB6C1","#FFA54F","#FF6666","#EEE685"]
-        let colorId=0;
-    
-        ob.sticky.style.background=colors[colorId]
-    
-    
+   
         ob.stickyText.addEventListener("input", function(e){
             ob.changeText();
             addToArray(ob.title,ob.text,ob.colorId);
@@ -117,19 +135,38 @@ function Note(_text, _title) {
     
             ob.sticky.style.background=colors[ob.colorId]
 
+            addToArray(ob.title,ob.text,ob.colorId);
+
         })
     
         ob.stickyDelete.addEventListener("click", function(e){
             
-            sticky.parentNode.removeChild(sticky)
-    
+            ob.sticky.parentNode.removeChild(ob.sticky)
+            delToArray()
+                
+        })
+
+        ob.stickyPinUp.addEventListener("click", function(e){
+            delToArray()
+            pinToArray(ob.title,ob.text,ob.colorId);
+            refreshSticky()
+
         })
     
 
-    function addToArray(title,text,colorId){
-        console.log("addToArray\ntitle: "+title+"\ntext: "+text)
-        stickyArray.splice(ob.ArrayIndex,1,[title,text,colorId]);
-    }
+        function addToArray(title,text,colorId){
+            stickyArray.splice(ob.ArrayIndex,1,[title,text,colorId])
+            saveLocal()
+        }
+
+        function delToArray(){
+            stickyArray.splice(ob.ArrayIndex,1)
+            saveLocal()
+        }
+        function pinToArray(title,text,colorId){
+            stickyArray.splice(0,0,[title,text,colorId])
+            saveLocal()
+        }
 
     return ob.sticky
     
